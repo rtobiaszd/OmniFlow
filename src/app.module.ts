2a4860d5f1316b2b13f89ec016763f2d93536499
Roberto Tobias Zikert Dalchau <roberto.dalchau@slmandic.edu.br>
2026-03-30 14:45:55 -0300

    eaaea

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
-import { Module } from '@nestjs/common';
+// Mock implementation of AppModule for testing purposes
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from './app.module';
 
-@Module({
-  imports: [],
-  controllers: [],
-  providers: []
-})
-export class AppModule {}
+describe('AppModule', () => {
+  let module: TestingModule;
+
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+  });
+
+  it('should compile', () => {
+    expect(module).toBeDefined();
+  });
+});
